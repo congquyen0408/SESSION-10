51b7a8530ac6fe8c3c0a2a7e3f7e0b1be88bb167
Le Chau Cong Quyen BRSE NI - 09 <congquyen0408@gmail.com>
2026-06-20 00:25:08 +0900

    Add files via upload

---
+++ baitap5/baitap5.js
@@ -0,0 +1,5 @@
+let usd = prompt("Nhập số tiền Đô la Mỹ USD");
+const Exchange = 25000;
+let vnd = usd * Exchange;
+alert(" $ " + usd + " = "+ vnd + "đ");
+console.log (" $ " + usd + " = "+ vnd + "đ");
---
+++ baitap6/baitap6.js
@@ -0,0 +1,8 @@
+let math = Number(prompt("Hãy nhập điểm Toán"));
+let physics = Number(prompt("Hãy nhập điểm Sinh"));
+let chemistry = Number(prompt("Hãy nhập điểm Hóa"));
+
+let trungBinh = (math + physics + chemistry)/3 ;
+
+console.log("Điểm trung bình là: " + trungBinh);
+alert("Điểm trung bình là: " + trungBinh);
